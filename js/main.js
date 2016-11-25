f022fae8b84ad35db6a409544f26a4d8ba47b224
mban <mark.ban@wup.hu>
2016-11-25 06:38:47 +0100

    bela

--- js/main.js
+++ js/main.js
@@ -167,7 +167,7 @@ function gameloop() {
    //create the bounding box
    var box = document.getElementById('player').getBoundingClientRect();
    var origwidth = 34.0;
-   var origheight = 24.0;
+   var origheight = 54.0;
    
    var boxwidth = origwidth - (Math.sin(Math.abs(rotation) / 90) * 8);
    var boxheight = (origheight + box.height) / 2;
